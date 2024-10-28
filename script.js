601323e16827777ba4a685bf5cc3847ee1ca7bcf
edvin <88817957+edvintually@users.noreply.github.com>
2024-10-27 21:26:24 -0700

    v0.1 the start
    github publish. index, contact, and 404 added.

---
+++ script.js
@@ -0,0 +1,4 @@
+function setHalfVolume() {
+    var myAudio = document.getElementById("lowval");  
+    myAudio.volume = 0.25; //Changed this to 0.5 or 50% volume since the function is called Set Half Volume ;)
+}
